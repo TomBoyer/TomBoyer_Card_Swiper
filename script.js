2df12a6cbe2cab74e725a44393516e7fd796010e
Tom Boyer <boyer.tom21@gmail.com>
2022-02-16 23:30:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// https://swiperjs.com/get-started#use-swiper-from-cdn
+//lien pour installer le script de swipe
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+});
